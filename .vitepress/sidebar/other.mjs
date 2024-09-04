f163d4607c3cd5fc4d66973c17bd1cb3d5279dde
chen <13439594353@163.com>
2024-09-04 14:23:56 +0800

    更新

--- .vitepress/sidebar/other.mjs
+++ .vitepress/sidebar/other.mjs
@@ -12,6 +12,7 @@ export default [
     collapsible: false, // 是否可折叠的侧边栏组
     collapsed: false, // 是否展开侧边栏组，默认展开，设置为 true 为隐藏
     items: [
+      { text: '系统配置', link: `${commonPath}/settings.md` },
       { text: '上传', link: `${commonPath}/upload.md` },
       { text: '查询选择', link: `${commonPath}/rhSelectListBox.md` },
       { text: 'wps在线编辑方式', link: `${commonPath}/wps.md` },
---
+++ other/settings.md
@@ -0,0 +1,9 @@
+# 系统配置
+
+## 获取系统变量
+```js
+import settings from 'rh-lib/src/settings'
+
+const store = settings.syStore
+const sysParams = store.getters.sysParams
+```
